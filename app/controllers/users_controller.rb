8bcd7c9f959c55094574d5a6cfe3eb4800022c14
z16019th <z16019th@aiit.ac.jp>
2019-05-19 13:51:42 +0000

    perform test for update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,8 +21,25 @@ class UsersController < ApplicationController
     end
   end
   
-  private
+  #GET /users/:id/edit
+  #params[:id] => :id
+  def edit
+    @user = User.find(params[:id])
+    # => edit.html.erb
+  end
   
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params) ##必要な項目だけUpdateするSQLがは行される
+      #Success
+    else
+      #Failure
+    # => @user.errors.full_messages()
+       render 'edit'
+    end
+  end
+  private
+  #この後はすべてprivateになる。これを上書きされないようにする。外側からは呼び出されない。
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
